487276e482a44f51b817a43948999d9893baa914
송정호 <dynamite885@naver.com>
2025-01-31 18:31:35 +0900

    feat: roomfurniture 도메인 파일 추가
    # Conflicts:
    #	gagunokuga/Gagunokuga_back/src/main/java/com/example/gagunokuga_back/roomfurniture/domain/RoomFurniture.java

--- gagunokuga/Gagunokuga_back/src/main/java/com/example/gagunokuga_back/roomfurniture/domain/RoomFurniture.java
+++ gagunokuga/Gagunokuga_back/src/main/java/com/example/gagunokuga_back/roomfurniture/domain/RoomFurniture.java
@@ -1,7 +1,5 @@
 package com.example.gagunokuga_back.roomfurniture.domain;
 
-import com.example.gagunokuga_back.furniture.domain.Furniture;
-import com.example.gagunokuga_back.room.domain.Room;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -37,12 +35,4 @@ public class RoomFurniture {
 
     @Column(nullable = false)
     private Boolean collapse;
-
-    @ManyToOne
-    @JoinColumn(name = "room_id")
-    private Room room;
-
-    @ManyToOne
-    @JoinColumn(name = "furniture_id")
-    private Furniture furniture;
 }
