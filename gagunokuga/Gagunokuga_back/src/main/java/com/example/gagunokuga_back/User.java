10e71c877bad7f4b61dd0ae5a2d0a303b911607f
g1767g <g1767g@naver.com>
2025-02-01 18:02:03 +0900

    hotfix: DB 설정 변경

--- gagunokuga/Gagunokuga_back/src/main/java/com/example/gagunokuga_back/User.java
+++ gagunokuga/Gagunokuga_back/src/main/java/com/example/gagunokuga_back/User.java
@@ -10,4 +10,7 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    private String name;
+    private String email;
+    private String password;
 }
