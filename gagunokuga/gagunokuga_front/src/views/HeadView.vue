06158f4ba5a00461c4ffefc167b8b6bf6f2e225b
yhkimx <yhkimx@naver.com>
2025-02-03 16:55:31 +0900

    프론트 적용
    (cherry picked from commit 0674ee05f26fd77e7b28cc7ecbebd68b002d71c5)

---
+++ gagunokuga/gagunokuga_front/src/views/HeadView.vue
@@ -0,0 +1,37 @@
+<template>
+  <header class="navbar">
+    <nav>
+      <ul>
+        <li><router-link to="/">메인</router-link></li>
+        <li><router-link to="/login">로그인</router-link></li>
+        <li><router-link to="/signup">회원가입</router-link></li>
+        <li><router-link to="/room">룸 소개</router-link></li>
+        <li><router-link to="/articles">게시판</router-link></li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.navbar {
+  background-color: black;
+  color: white;
+  padding: 10px;
+}
+.navbar ul {
+  list-style: none;
+  display: flex;
+  gap: 15px;
+  margin: 0;
+  padding: 0;
+}
+.navbar a,
+.navbar .router-link-active {
+  color: white;
+  text-decoration: none;
+}
+</style>
