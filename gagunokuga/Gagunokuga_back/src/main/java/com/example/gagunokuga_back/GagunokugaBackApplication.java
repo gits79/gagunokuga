a86908938e6feae2b69ab98f4e5230d8b66d89de
김예훈 <yhkimx@naver.com>
2025-02-04 13:11:18 +0900

    프론트엔드 기초
    (cherry picked from commit 5571fb9849cc677fa169cfe921f198741a07bb34)

--- gagunokuga/Gagunokuga_back/src/main/java/com/example/gagunokuga_back/GagunokugaBackApplication.java
+++ gagunokuga/Gagunokuga_back/src/main/java/com/example/gagunokuga_back/GagunokugaBackApplication.java
@@ -2,10 +2,10 @@ package com.example.gagunokuga_back;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-@EnableJpaAuditing
+@EntityScan(basePackages = "com.example.gagunokuga_back") // BaseTimeEntity가 위치한 패키지
 public class GagunokugaBackApplication {
 
     public static void main(String[] args) {
