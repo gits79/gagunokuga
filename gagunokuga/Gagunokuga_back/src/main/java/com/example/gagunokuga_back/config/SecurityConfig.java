daa12c968f20676225738fbe48be409914ab3771
daeun <daeunchang@naver.com>
2025-02-06 10:16:09 +0900

    cors 설정

--- gagunokuga/Gagunokuga_back/src/main/java/com/example/gagunokuga_back/config/SecurityConfig.java
+++ gagunokuga/Gagunokuga_back/src/main/java/com/example/gagunokuga_back/config/SecurityConfig.java
@@ -19,6 +19,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -38,6 +43,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtTokenProvider jwtTokenProvider) throws Exception {
         return http
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 // REST API.. csrf 보안 x
                 .csrf(csrf ->csrf.disable())
                 // JWT를 사용하기 때문에 세션x
@@ -57,6 +63,20 @@ public class SecurityConfig {
 //                .requestMatchers("/members/test").hasRole("USER")
 
 
+    }
+
+
+    //cors 설정
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
 
