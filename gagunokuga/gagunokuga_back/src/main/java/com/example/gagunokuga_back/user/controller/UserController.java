4a0b104d0141270412d06aa153341a8b4b4220a9
daeun <daeunchang@naver.com>
2025-02-06 17:57:42 +0900

    resetPwd

--- gagunokuga/gagunokuga_back/src/main/java/com/example/gagunokuga_back/user/controller/UserController.java
+++ gagunokuga/gagunokuga_back/src/main/java/com/example/gagunokuga_back/user/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.example.gagunokuga_back.user.controller;
 
 import com.example.gagunokuga_back.user.dto.PasswordRequestDto;
+import com.example.gagunokuga_back.user.dto.PasswordResetRequestDto;
 import com.example.gagunokuga_back.user.dto.UpdateRequestDto;
 import com.example.gagunokuga_back.user.dto.UserResponseDto;
 import com.example.gagunokuga_back.user.service.UserService;
@@ -36,12 +37,13 @@ public class UserController {
         return ResponseEntity.ok().build();
     }
 
-//    //비밀번호 재설정 (잃어버렸을때)
-//    @PostMapping("/pwd/reset")
-//    public ResponseEntity<Void> resetPassword(@RequestBody PasswordResetRequestDto passwordResetDto) {
-//        userService.reset(passwordResetDto);
-//        return ResponseEntity.ok().build();
-//    }
+
+    //비밀번호 재설정 (잃어버렸을때) -- 현재는 임시비번 유효시간 설정 안해놓음, boolean 체킹으로 백엔드에서 무조건 수정해야지만 페이지 사용할 수 있게 처리할지,,
+    @PostMapping("/pwd/reset")
+    public ResponseEntity<?> resetPassword(@RequestBody PasswordResetRequestDto passwordResetDto) {
+        userService.reset(passwordResetDto);
+        return ResponseEntity.ok("Email sent");
+    }
 
     //탈퇴
     @DeleteMapping
