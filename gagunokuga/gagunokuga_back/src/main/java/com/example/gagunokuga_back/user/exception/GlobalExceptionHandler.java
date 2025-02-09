df6789dc4552224dbdbf7a658e6f7fc75503640d
boriaegi <daeunchang@naver.com>
2025-02-09 11:02:47 +0900

    add exception package

---
+++ gagunokuga/gagunokuga_back/src/main/java/com/example/gagunokuga_back/user/exception/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.example.gagunokuga_back.user.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    // UserNotFoundException 처리
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<String> handleUserNotFoundException(UserNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    // 일반적인 예외 처리
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGeneralException(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("서버 오류가 발생했습니다.");
+    }
+}
