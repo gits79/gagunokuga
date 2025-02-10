dd0aa3a4335750bd6e154ed6f15978fc2c3e0ee1
김민섭 <qwedsaszxc@naver.com>
2025-02-10 14:05:22 +0900

    feat : 에디터 모듈화 준비

--- gagunokuga/gagunokuga_front/src/router/index.js
+++ gagunokuga/gagunokuga_front/src/router/index.js
@@ -2,15 +2,15 @@ import { createRouter, createWebHashHistory } from "vue-router";
 import Home from "@/views/home/Home.vue";
 import Login from "@/views/login/Login.vue";
 import SignUp from "@/views/signup/SignUp.vue";
-import RoomList from "@/views/room-list/RoomList.vue";
-import FloorPlanEditor from "@/views/floor-plan-editor/FloorPlanEditor.vue";
+import RoomList from "@/views/room/Room.vue";
+import FloorPlanEditor from "@/views/editor/Editor.vue";
 
 const routes = [
   { path: "/", name: "Home", component: Home },
   { path: "/login", name: "Login", component: Login },
   { path: "/signup", name: "SignUp", component: SignUp },
-  { path: "/room-list", name: "RoomList", component: RoomList },
-  { path: '/editor/:roomId', name: 'FloorPlanEditor', component: FloorPlanEditor },
+  { path: "/room", name: "Room", component: RoomList },
+  { path: '/editor/:roomId', name: 'Editor', component: FloorPlanEditor },
 ];
 
 const router = createRouter({
