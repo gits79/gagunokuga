387f9eecdaf7289d8a11113156e5afa960193206
unknown <yoonji0624@naver.com>
2025-02-11 14:35:53 +0900

    [S12P11A607-83] modify ArticleList.vue

--- gagunokuga/gagunokuga_front/src/views/article/ArticleList.vue
+++ gagunokuga/gagunokuga_front/src/views/article/ArticleList.vue
@@ -1,5 +1,4 @@
 <template>
-<<<<<<< HEAD
   <div class="container mx-auto p-4 flex justify-center">
     <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
       <div v-for="article in store.articleList.articles" :key="article.id" 
@@ -41,33 +40,3 @@ onMounted(() => {
   overflow: hidden;
 }
 </style>
-=======
-    <!-- 두 번째 줄 (24개 박스) -->
-    <div class="row grid grid-cols-4 gap-4 max-w-screen-lg w-full p-2">
-      <div v-for="(article, index) in store.articleList.articles" :key="index" class="box bg-gray-300 flex justify-center items-center aspect-square w-full overflow-hidden">
-        <div class="w-full h-full flex justify-center items-center">
-          <div class="text-center">
-            <div class="text-lg font-bold">{{ article.title }}</div>
-            <div class="text-sm">{{ article.content }}</div>
-          </div>
-        </div>
-      </div>
-    </div>
-</template>
-
-<script setup>
-    import { ref, onMounted } from 'vue';
-import { useArticleStore } from './articleStore';
-
-    const store = useArticleStore();
-
-    onMounted(() => {
-        store.getArticleList();
-    });
-
-</script>
-
-<style scoped>
-
-</style>
->>>>>>> a9ffdbc4345b24c7244a92f3e4009161d8d08bb0
