d71df3d1e39b02b2218a94e3a3fc9658cfe756be
unknown <yoonji0624@naver.com>
2025-02-11 10:39:58 +0900

    [S12P11A607-78]update AWS SDK

--- gagunokuga/gagunokuga_back/src/main/java/com/example/gagunokuga_back/config/S3Config.java
+++ gagunokuga/gagunokuga_back/src/main/java/com/example/gagunokuga_back/config/S3Config.java
@@ -3,6 +3,7 @@ package com.example.gagunokuga_back.config;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.providers.DefaultAwsRegionProviderChain;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.regions.Region;
 import org.springframework.beans.factory.annotation.Value;
@@ -22,6 +23,7 @@ public class S3Config {
     @Bean
     public S3Client s3Client() {
         AwsBasicCredentials awsCreds = AwsBasicCredentials.create(accessKey, secretKey);
+
         return S3Client.builder()
                 .region(Region.of(region))
                 .credentialsProvider(StaticCredentialsProvider.create(awsCreds))
