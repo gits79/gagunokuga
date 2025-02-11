1c0aadc728038c4960d22bef6e0bf821ff9f7dcd
송정호 <dynamite885@naver.com>
2025-02-11 19:33:48 +0900

    feat: RoomUser 관련 DTO 추가

---
+++ gagunokuga/gagunokuga_back/src/main/java/com/example/gagunokuga_back/roomuser/dto/KickGuestRequest.java
@@ -0,0 +1,10 @@
+package com.example.gagunokuga_back.roomuser.dto;
+
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+public class KickGuestRequest {
+    private String nickname;
+}
