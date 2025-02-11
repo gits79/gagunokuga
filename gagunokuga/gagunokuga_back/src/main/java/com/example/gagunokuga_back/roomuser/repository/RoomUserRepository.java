854d482efeca0804bf0bbce07b10ff0a12c085f7
송정호 <dynamite885@naver.com>
2025-02-11 19:34:21 +0900

    feat: RoomUser 서비스 로직 추가

---
+++ gagunokuga/gagunokuga_back/src/main/java/com/example/gagunokuga_back/roomuser/repository/RoomUserRepository.java
@@ -0,0 +1,29 @@
+package com.example.gagunokuga_back.roomuser.repository;
+
+import com.example.gagunokuga_back.room.domain.Room;
+import com.example.gagunokuga_back.roomuser.domain.RoomUser;
+import com.example.gagunokuga_back.user.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface RoomUserRepository extends JpaRepository<RoomUser, Long> {
+    boolean existsByRoomAndUser(Room room, User user);
+
+    List<RoomUser> findAllByRoom(Room room);
+
+    RoomUser findByRoomAndUser(Room room, User user);
+
+    void deleteAllByRoom(Room room);
+
+    @Query("SELECT r.room FROM RoomUser r WHERE r.user = :user")
+    List<Room> selectAllByUser(@Param("user") User user);
+
+    @Query("SELECT r.room FROM RoomUser r WHERE r.user = :user AND r.isHost = true")
+    List<Room> selectAllByUserAndIsHostIsTrue(@Param("user") User user);
+
+    @Query("SELECT r.room FROM RoomUser r WHERE r.user = :user AND r.isHost = false")
+    List<Room> selectAllByUserAndIsHostIsFalse(@Param("user") User user);
+}
