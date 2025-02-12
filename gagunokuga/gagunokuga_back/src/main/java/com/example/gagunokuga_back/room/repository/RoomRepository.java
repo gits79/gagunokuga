17b0635346a81c6f8f2e3ab1d065a85712698625
g1767g <g1767g@naver.com>
2025-02-12 14:23:38 +0900

    feat: 방 삭제 및 현재 사용자 방 목록 - 방 삭제 안되는 오류 해결 연관관계 때문에 룸 사용자가 먼저 삭제되고 룸이 삭제되어야 함 - 현재 사용자의 룸들만 보여주기 위해 JPQUERY 추가 - 현재 사용자 가져오고 현재 사용자의 방만 가져오는 걸로 수정

--- gagunokuga/gagunokuga_back/src/main/java/com/example/gagunokuga_back/room/repository/RoomRepository.java
+++ gagunokuga/gagunokuga_back/src/main/java/com/example/gagunokuga_back/room/repository/RoomRepository.java
@@ -1,7 +1,14 @@
 package com.example.gagunokuga_back.room.repository;
 
 import com.example.gagunokuga_back.room.domain.Room;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface RoomRepository extends JpaRepository<Room, Long> {
+    // 전체 룸 중에 현재 사용자가 있는 방들을 찾음
+    @Query("SELECT r FROM Room r JOIN RoomUser ru ON r.id = ru.room.id WHERE ru.user.id = :userId")
+    Page<Room> findRoomsByUserId(@Param("userId") Long userId, Pageable pageable);
 }
