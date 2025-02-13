3edb8f5de34f4101746ce36014a676cc7ea3a6e0
yhkimx <yhkimx@naver.com>
2025-02-13 17:02:29 +0900

    마이페이지 수정중 전체 웹페이지 높이 조정중

--- gagunokuga/gagunokuga_front/src/views/mypage/Mypage.vue
+++ gagunokuga/gagunokuga_front/src/views/mypage/Mypage.vue
@@ -2,8 +2,17 @@
   <div class="my-page">
     <!-- 사용자 정보 표시 -->
     <div class="content-wrapper">
-      <div class="user-info">
-        <img :src="profileImage" alt="Profile Image" class="profile-image"/>
+      <div class="profile-changer">
+        <div class="user-info">
+          <img :src="profileImage" alt="Profile Image" class="profile-image"/>
+        </div>
+        <button @click="updateProfileImage" class="blue-btn">
+          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="btn-icon">
+            <path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z"/>
+            <circle cx="12" cy="13" r="4"/>
+          </svg>
+          프로필 수정
+        </button>
       </div>
       <div class="mypage-info">
         <router-view></router-view>  <!-- 자식 컴포넌트를 여기 표시 -->
@@ -29,8 +38,23 @@ const router = useRouter();
 const baseURL = import.meta.env.VITE_API_URL;
 
 // 프로필 수정 페이지로 이동
-const goToProfileEdit = () => {
-  router.push("/mypage/update");
+const updateProfileImage = async (file) => {
+  const formData = new FormData();
+  formData.append("profileImage", file);
+
+  try {
+    const response = await axios.put(`${baseURL}/api/users/profile-image`, formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+        Authorization: `Bearer ${loginStore.state.token}`,
+      },
+    });
+    alert("프로필 이미지가 성공적으로 수정되었습니다.");
+    // await loginStore.fetchUserInfo();
+    loginStore.setUserInfo(response.data); // 사용자 정보 업데이트
+  } catch (error) {
+    console.error("프로필 이미지 수정 오류", error);
+  }
 };
 
 // 정보 수정 페이지로 이동
