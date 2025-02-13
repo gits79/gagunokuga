f3b0c00cd5243987345179f8114fd9e1ff64d0a1
unknown <yoonji0624@naver.com>
2025-02-13 18:00:11 +0900

    메인페이지 반응형

--- gagunokuga/gagunokuga_front/src/router/index.js
+++ gagunokuga/gagunokuga_front/src/router/index.js
@@ -14,6 +14,7 @@ import ArticleDetail from "../views/article/ArticleDetail.vue";
 import ArticleCreate from "@/views/article/ArticleCreate.vue";
 import MypageInfo from "@/views/mypage/mypage/MypageInfo.vue";
 import MypageUpdate from "@/views/mypage/mypage/MypageUpdate.vue";
+import Test from "@/views/article/Test.vue";
 
 
 const router = createRouter({
@@ -43,9 +44,14 @@ const router = createRouter({
           path: "article/:articleId/edit",
           name: "ArticleUpdate",
           component: ArticleCreate
-        }
+        },
       ],
       meta: { showHeader: true },
+    },
+    {
+      path: "/test",
+      name: "Test",
+      component: Test
     },
     
     { path: "/login", name: "Login", component: Login, meta: { showHeader: true }, },
