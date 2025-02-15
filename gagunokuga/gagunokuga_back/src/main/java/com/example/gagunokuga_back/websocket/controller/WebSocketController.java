5aa1400aa0c778d4cfdb8b6de1848784123cc8d2
송정호 <dynamite885@naver.com>
2025-02-15 18:58:46 +0900

    chore: 사용되지 않는 부분 삭제

--- gagunokuga/gagunokuga_back/src/main/java/com/example/gagunokuga_back/websocket/controller/WebSocketController.java
+++ gagunokuga/gagunokuga_back/src/main/java/com/example/gagunokuga_back/websocket/controller/WebSocketController.java
@@ -1,10 +1,8 @@
 package com.example.gagunokuga_back.websocket.controller;
 
-
-import com.example.gagunokuga_back.roomfurniture.service.RoomFurnitureService;
 import com.example.gagunokuga_back.websocket.dto.FurnitureEventDto;
+import com.example.gagunokuga_back.websocket.service.WebSocketService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
@@ -17,11 +15,11 @@ public class WebSocketController {
     private final SimpMessageSendingOperations template;
 
     @MessageMapping("/rooms/{roomId}")
-    public ResponseEntity<Void> receiveFurnitureInfo(
+    public void receiveFurnitureInfo(
             @DestinationVariable Long roomId,
             FurnitureEventDto furnitureEventDto) {
         System.out.println(furnitureEventDto);
         template.convertAndSend("/sub/rooms/" + roomId, furnitureEventDto);
-        return ResponseEntity.ok().build();
+        webSocketService.receiveFurnitureEvent(roomId, furnitureEventDto);
     }
 }
