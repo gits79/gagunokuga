7f23e39c3218ba6295e286ccac4f1d699edb9888
yhkimx <yhkimx@naver.com>
2025-02-18 15:58:50 +0900

    기존 화면캡쳐를 위해 수행했던 불필요해진 코드들 정리

--- gagunokuga/gagunokuga_back/src/main/java/com/example/gagunokuga_back/image/controller/ImageController.java
+++ gagunokuga/gagunokuga_back/src/main/java/com/example/gagunokuga_back/image/controller/ImageController.java
@@ -2,18 +2,9 @@ package com.example.gagunokuga_back.image.controller;
 
 import com.example.gagunokuga_back.image.service.ImageService;
 import lombok.RequiredArgsConstructor;
-import org.openqa.selenium.By;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.*;
-import java.nio.file.Files;
-import java.util.NoSuchElementException;
 
 @RestController
 @RequestMapping("/api/image")
@@ -31,40 +22,6 @@ public class ImageController {
         } catch (Exception e) {
             System.out.println(e.getMessage());
             return "File upload failed!";
-        }
-    }
-
-    @GetMapping("/captureElement")
-    public @ResponseBody byte[] captureElement(@RequestParam String url, @RequestParam String elementClass) throws IOException {
-        System.out.println(url + " / " + elementClass + " / 캡쳐링 확인");
-        System.setProperty("webdriver.chrome.driver", "/path/to/chromedriver"); // chromedriver 경로 설정
-
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless"); // 브라우저 창 없이 실행
-        WebDriver driver = new ChromeDriver(options);
-
-        driver.get(url);
-
-        try {
-            // 특정 클래스명 요소 찾기
-            WebElement element = driver.findElement(By.className(elementClass));
-
-            // 요소 캡처
-            File screenshot = element.getScreenshotAs(OutputType.FILE);
-
-            // 파일로 저장하기
-            File destinationFile = new File("C:/SSAFY/S12P11A607/gagunokuga");  // 원하는 경로로 설정
-            Files.copy(screenshot.toPath(), destinationFile.toPath());   // 이미지 파일 복사
-
-            // 파일을 byte[]로 읽기 (필요한 경우)
-            byte[] imageBytes = Files.readAllBytes(destinationFile.toPath());
-
-            driver.quit();
-            return imageBytes;
-
-        } catch (NoSuchElementException e) {
-            driver.quit();
-            throw new RuntimeException("요소를 찾을 수 없습니다: " + elementClass);
         }
     }
 }
