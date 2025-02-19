7c4377a764b0c01d218c4c2dabe9010cb8c93b8b
g1767g <g1767g@naver.com>
2025-02-19 22:59:27 +0900

    방 생성 후 바로 에디터로 들어가기 위해서 벡엔드 수정

--- gagunokuga/gagunokuga_back/src/main/java/com/example/gagunokuga_back/room/service/RoomService.java
+++ gagunokuga/gagunokuga_back/src/main/java/com/example/gagunokuga_back/room/service/RoomService.java
@@ -2,11 +2,12 @@ package com.example.gagunokuga_back.room.service;
 
 import com.example.gagunokuga_back.room.domain.Room;
 import com.example.gagunokuga_back.room.dto.RoomListResponse;
+import com.example.gagunokuga_back.room.dto.RoomResponse;
 import com.example.gagunokuga_back.room.dto.UpdateRoomNameRequest;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface RoomService {
-    public void createRoom(String roomName, String thumbnailUrl);
+    public RoomResponse createRoom(String roomName, String thumbnailUrl);
     public RoomListResponse getRoomList(int page);
     public void updateRoomName(Long roomId, UpdateRoomNameRequest updateRoomNameRequest);
     public void deleteRoom(Long roomId);
