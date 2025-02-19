c39c0f6248d25ad51feef78f20725d8a2baa788c
g1767g <g1767g@naver.com>
2025-02-19 22:25:21 +0900

    헤더홈 만들기 변경

--- gagunokuga/gagunokuga_front/src/components/the-header/TheHeader.vue
+++ gagunokuga/gagunokuga_front/src/components/the-header/TheHeader.vue
@@ -1,12 +1,14 @@
 <script setup>
 import { useRoomListStore } from "../../views/room/roomStore";
 import { useLoginStore } from "@/views/login/login";  // 로그인 상태를 가져옵니다
-import { computed, watchEffect } from "vue";
+import { computed, watchEffect, ref } from "vue";
 import { useRouter } from "vue-router";
+import CreateRoomModal from "../../views/room/CreateRoomModal.vue";
 
 const loginStore = useLoginStore();  // 로그인 상태 초기화
 const roomStore = useRoomListStore();
 const router = useRouter();
+const isModalOpen = ref(false); 
 
 // 로그인 상태에 따라 메뉴 항목을 동적으로 처리
 const isLoggedIn = computed(() => !!loginStore.state.token);  // 로그인 상태 판단
@@ -26,29 +28,29 @@ const logout = () => {
   loginStore.logout();  // loginStore의 logout 메서드 호출
 };
 
-const handleCreateRoom = async () => {
-
-  if(!isLoggedIn.value){
-    router.push({ name: 'Login'})
+// 방 생성 모달 열기
+const handleCreateRoom = () => {
+  if (!isLoggedIn.value) {
+    router.push({ name: 'Login' });
   } else {
-    const name = prompt('새로운 방 이름을 입력하세요:')
-    if (name?.trim()) {
-      await roomStore.createRoom(name.trim());
-      await roomStore.fetchRooms();
-      const roomId = roomStore.rooms.at(-1).roomId;
-      router.push({ name: 'FloorEditor', params: { roomId } })
-    }
+    isModalOpen.value = true;
   }
-}
+};
+
+// 방 생성 후 모달 닫기
+const handleRoomCreated = async (roomName) => {
+  await roomStore.createRoom(roomName);
+  await roomStore.fetchRooms();
+  isModalOpen.value = false;
+};
 
 const handleMyRoom = async () => {
-  if (!isLoggedIn.value){
-    router.push({ name: 'Login'})
+  if (!isLoggedIn.value) {
+    router.push({ name: 'Login' });
   } else {
-    router.push({ name: 'Room' })
+    router.push({ name: 'Room' });
   }
 }
-
 </script>
 
 <template>
@@ -84,11 +86,12 @@ const handleMyRoom = async () => {
         <li v-if="isLoggedIn" @click="logout" class="logout"><router-link to="/login">로그아웃</router-link></li>
       </ul>
     </nav>
+
+    <!-- 방 생성 모달 -->
+    <CreateRoomModal v-if="isModalOpen" @close="isModalOpen = false" @create="handleRoomCreated" />
   </header>
 </template>
 
-
 <style scoped>
 @import "./theHeader.css";
-
 </style>
